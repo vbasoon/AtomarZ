e303443a6b62ca944810ee592b97aa75cb46a93e
Басун Володимир <vbasoon@gmail.com>
2021-04-13 13:41:16 +0300

    first

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+  <div class="container">
+    <div>
+      <Header />
+      <h1 class="title">
+        atomars
+      </h1>
+      
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.wrapper {
+  max-width: 500px;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  border: ;
+}
